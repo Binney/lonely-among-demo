7f8c0f0d980c78d036a13356d66d374b1ba1e518
Sarah Binney <sarah.binney@softwire.com>
2024-03-29 16:04:59 +0000

    now walk properly

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = 4000.0
+const JUMP_VELOCITY = -400.0
+const FLOOR_FRICTION = 100.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var base_gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,22 +13,22 @@ func _physics_process(delta):
 	up_direction = -position.normalized()
 	rotation = position.angle() - PI / 2
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		if not $AnimatedSprite2D.is_playing():
-			$AnimatedSprite2D.play()
-		var x_velocity = direction * SPEED
-		velocity = position.normalized().rotated(-PI / 2) * x_velocity
-	else:
-		var x_velocity = 0
-		velocity = position.normalized().rotated(-PI / 2) * x_velocity
-		if $AnimatedSprite2D.is_playing():
-			$AnimatedSprite2D.stop()
+	var local_velocity = velocity.rotated(-rotation)
 
 	if not is_on_floor():
-		var gravity = base_gravity * position / gravity_normal_distance
-		velocity += gravity * delta
+		local_velocity.y += base_gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		local_velocity.y = JUMP_VELOCITY
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		local_velocity.x = direction * SPEED
+		$AnimatedSprite2D.play()
+	else:
+		local_velocity.x = move_toward(local_velocity.x, 0, FLOOR_FRICTION)
+		$AnimatedSprite2D.stop()
+
+	velocity = local_velocity.rotated(rotation)
 
 	move_and_slide()
